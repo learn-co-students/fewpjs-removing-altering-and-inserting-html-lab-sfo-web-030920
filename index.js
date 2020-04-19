bb178418b508de8290126d3d93ca94527c287a5a
caicindy87 <caicindy87@gmail.com>
2020-04-19 11:08:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your code here!
+let element = document.createElement('div')
+document.body.appendChild(element)
+
+let ul = document.createElement('ul')
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+
+element.appendChild(ul)
+
+element.style.backgroundColor = '#27647B';
+element.style.textAlign = 'center';
+ul.style.textAlign = 'left'
+
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'C is the champion';
